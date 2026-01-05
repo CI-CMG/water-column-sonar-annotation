81f70e0ac8af9cb7a363d71be368be9035611a9e
oftfrfbf <rudyklucik@gmail.com>
2026-01-05 12:41:32 -0700

    resolved package installations and added test directory

---
+++ tests/test_process_evr_files.py
@@ -0,0 +1,16 @@
+input_bucket_name = "example_input_bucket"
+output_bucket_name = "example_output_bucket"
+
+
+#######################################################
+def setup_module():
+    print("setup")
+
+
+def teardown_module():
+    print("teardown")
+
+
+#######################################################
+def test_process_evr_files(tmp_path):
+    pass
